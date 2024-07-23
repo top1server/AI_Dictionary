f716994785eaab2d5b6d0fd272825b1288ffa79d
top1server <mightdung7105@gmail.com>
2024-07-23 08:38:25 +0700

    TDM

---
+++ src/main/java/app/Run/Run.java
@@ -0,0 +1,9 @@
+package app.Run;
+
+import app.SourceCode.FileActivities.WriteToFile;
+
+public class Run {
+    public static void main(String[] args) {
+        WriteToFile.writeTXT("src/main/resources/txt/dictionary_add.txt");
+    }
+}
